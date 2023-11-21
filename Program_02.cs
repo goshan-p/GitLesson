409fc8594b62aafae6c44f37b5b1547ed164dda7
goshan-p <146089149+goshan-p@users.noreply.github.com>
2023-11-21 22:55:26 +0300

    Add files via upload

---
+++ Program_02.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+namespace HW1
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b;
+
+            try
+            {
+                Console.WriteLine("Var 1:");
+                a = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Var 2:");
+                b = double.Parse(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Plese enter only numbers");
+                return;
+            }
+
+            Console.WriteLine("Sum: {0}", a + b);
+            Console.WriteLine("Diff: {0}", a - b);
+            Console.WriteLine("Mult: {0}", a * b);
+
+            if (b == 0)
+                Console.WriteLine("Div: Error");
+            
+            else
+                Console.WriteLine("Div: {0}", a / b);
+        }
+    }
+}
