4a0909aa1fa6f2342cf456faab9e41ed288b5ca1
goshan-p <146089149+goshan-p@users.noreply.github.com>
2023-11-22 23:08:51 +0300

    Add files via upload

---
+++ Calc_03.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace HW1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b;
+            bool aValid, bValid;
+
+            while (true)
+            {
+                do
+                {
+                    Console.WriteLine("Var 1:");
+                    aValid = double.TryParse(Console.ReadLine(), out a);
+
+                    if (!aValid)
+                    {
+                        Console.WriteLine("Invalid input for variable 1. Please enter a valid number.");
+                    }
+                } while (!aValid);
+
+                do
+                {
+                    Console.WriteLine("Var 2:");
+                    bValid = double.TryParse(Console.ReadLine(), out b);
+                    if (!bValid)
+                    {
+                        Console.WriteLine("Invalid input for variable 2. Please enter a valid number.");
+                    }
+                } while (!bValid);
+
+                Console.WriteLine("Sum: {0}", a + b);
+                Console.WriteLine("Diff: {0}", a - b);
+                Console.WriteLine("Mult: {0}", a * b);
+
+                if (b != 0)
+                    Console.WriteLine("Div: {0}", a / b);
+                else
+                    Console.WriteLine("Div: Error ");
+
+                Console.WriteLine("-----");
+            }
+        }
+    }
+}
